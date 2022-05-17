bf8944a5fdf9765a9ec2c63fcc6d128479a143df
kestrel <40023681+kPride5858@users.noreply.github.com>
2022-05-17 10:40:56 +0900

    Add files via upload

---
+++ decimalConverter/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace decimalConverter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string [] a = Console.ReadLine().Split();
+            int notation = int.Parse(a[1]) , decim = int.Parse(a[2]);
+            if (notation != 10)
+            {
+                Console.Write(Convert.ToInt64(a[0],notation));
+            }
+            else 
+            {
+                Console.Write(Convert.ToString(int.Parse(a[0]), decim));
+            }
+
+        }
+    }
+}
