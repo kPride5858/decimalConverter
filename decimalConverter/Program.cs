68d3cab3f853ef360cfdcf3fa89c37de179456eb
kestrel <40023681+kPride5858@users.noreply.github.com>
2022-05-17 14:21:34 +0900

    Update Program.cs

--- decimalConverter/Program.cs
+++ decimalConverter/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace decimalConverter
 {
@@ -6,17 +6,19 @@ namespace decimalConverter
     {
         static void Main(string[] args)
         {
-            string [] a = Console.ReadLine().Split();
-            int notation = int.Parse(a[1]) , decim = int.Parse(a[2]);
+            Console.WriteLine("변환할수? 변환할수의진수? 어떤진수로변환?");
+            string[] a = Console.ReadLine().Split();
+            int notation = int.Parse(a[1]), decim = int.Parse(a[2]);
             if (notation != 10)
             {
-                Console.Write(Convert.ToInt64(a[0],notation));
+                Console.Write("결과 : {0}", Convert.ToString(Convert.ToInt32(a[0], notation), decim));
             }
-            else 
+            else
             {
-                Console.Write(Convert.ToString(int.Parse(a[0]), decim));
+                Console.Write("결과 : {0}", Convert.ToString(int.Parse(a[0]), decim));
             }
 
         }
     }
 }
+    
