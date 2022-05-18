d1977b2718727a646967ae96167900668ba76a74
kestrel <40023681+kPride5858@users.noreply.github.com>
2022-05-18 13:39:14 +0900

    코드 간결화

--- decimalConverter/Program.cs
+++ decimalConverter/Program.cs
@@ -9,16 +9,9 @@ namespace decimalConverter
             Console.WriteLine("변환할수? 변환할수의진수? 어떤진수로변환?");
             string[] a = Console.ReadLine().Split();
             int notation = int.Parse(a[1]), decim = int.Parse(a[2]);
-            if (notation != 10)
-            {
-                Console.Write("결과 : {0}", Convert.ToString(Convert.ToInt32(a[0], notation), decim));
-            }
-            else
-            {
-                Console.Write("결과 : {0}", Convert.ToString(int.Parse(a[0]), decim));
-            }
+
+            Console.Write("결과 : {0}", Convert.ToString(Convert.ToInt32(a[0], notation), decim));
 
         }
     }
 }
-    
